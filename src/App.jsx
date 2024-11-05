653007dc1ccc99cc87816b36f5b75521ee9fa12c
kvnq-poza <kvnqpoza@gmail.com>
2024-11-05 17:37:10 +0100

    Updated routes and added Route based title updater for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Help from "./pages/Help";
 import Footer from "./components/Footer";
 import "./App.css";
 import GrowYoutube from "./pages/GrowYoutube";
+import { useEffect } from "react";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
+      <RouteBasedTitleUpdater />
       <div className="app">
         <Header />
         <Routes>
@@ -18,8 +20,30 @@ function App() {
         </Routes>
         <Footer />
       </div>
-    </Router>
+    </BrowserRouter>
   );
+}
+
+function RouteBasedTitleUpdater() {
+  const location = useLocation();
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/":
+        document.title = "CollectYoutube | Free YouTube Video Downloader";
+        break;
+      case "/help":
+        document.title = "Help - CollectYoutube";
+        break;
+      case "/grow-youtube":
+        document.title = "Grow Your YouTube Channel - CollectYoutube";
+        break;
+      default:
+        document.title = "CollectYoutube | Free YouTube Video Downloader";
+    }
+  }, [location]);
+
+  return null;
 }
 
 export default App;
