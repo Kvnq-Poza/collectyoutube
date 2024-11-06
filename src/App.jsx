e0d197d8b8c18295faaef1d8f00381b17e0cbb95
kvnq-poza <kvnqpoza@gmail.com>
2024-11-06 07:14:53 +0100

    Added lazy loading and suspense for page router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { lazy, Suspense, useEffect } from "react";
+
 import Header from "./components/Header";
-import Home from "./pages/Home";
-import Help from "./pages/Help";
+const Home = lazy(() => import("./pages/Home"));
+const Help = lazy(() => import("./pages/Help"));
+const GrowYoutube = lazy(() => import("./pages/GrowYoutube"));
 import Footer from "./components/Footer";
 import "./App.css";
-import GrowYoutube from "./pages/GrowYoutube";
-import { useEffect } from "react";
 
 function App() {
   return (
     <BrowserRouter>
       <RouteBasedTitleUpdater />
-      <div className="app">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="help" element={<Help />} />
-          <Route path="grow-youtube" element={<GrowYoutube />} />
-        </Routes>
-        <Footer />
-      </div>
+      <Suspense>
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="help" element={<Help />} />
+            <Route path="grow-youtube" element={<GrowYoutube />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Suspense>
     </BrowserRouter>
   );
 }
